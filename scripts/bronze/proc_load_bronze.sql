ebc8fc72cd14585d8589e23fbb70aa1b89a14ef2
Ric Araojo <ojoararico.me@gmail.com>
2025-09-12 17:46:44 +0800

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,61 @@
+/*
+=================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+=================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
+    It performs the following actions:
+    - Truncate the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None.
+    This stored procedure does not accept any parameter or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;   
+=================================================================
+*/
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '==============================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '==============================================';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.nat_inventory_target';
+		TRUNCATE TABLE bronze.nat_inventory_target; 
+		PRINT '>> Insert Table: bronze.nat_inventory_target';
+		BULK INSERT bronze.nat_inventory_target
+		FROM "C:\Users\ojoar\Desktop\data_set\ph_imf_climate_data\Philippines_PH_All_Indicators\03_nat_inventory_target.csv"
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' second';
+		PRINT '----------------------------------------------';
+
+
+		SET @batch_end_time = GETDATE();
+		PRINT '==============================================';
+		PRINT 'Loading Bronze Layer is Completed';
+		PRINT '  - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '==============================================';
+	END TRY
+
+	-- Script in case of Error in the Bronze Layer
+	BEGIN CATCH
+		PRINT '==============================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONCE LAYER';
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '==============================================';
+
+	END CATCH
+END
