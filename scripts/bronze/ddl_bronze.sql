581b346c9588e25a11bec23aade011620c5176d7
Ric Araojo <ojoararico.me@gmail.com>
2025-09-13 12:58:44 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -16,9 +16,8 @@ IF OBJECT_ID('bronze.ghg_co2_emissions', 'U') IS NOT NUll
 GO
   
 CREATE TABLE bronze.ghg_co2_emissions(
-  country NVARCHAR(15),	iso2 NVARCHAR(10),	iso3 NVARCHAR(10),	indicator_used NVARCHAR(50),
-  unit NVARCHAR(50),	source NVARCHAR(MAX),	cts_code NVARCHAR(15),	cts_name NVARCHAR(50),
-  cts_full_descriptor	NVARCHAR(MAX), industry NVARCHAR(MAX),	scale NVARCHAR(10),
+  country NVARCHAR(15),	iso2 NVARCHAR(10),	iso3 NVARCHAR(10),	indicator_used NVARCHAR(50), unit NVARCHAR(50), 
+  cts_code NVARCHAR(15),	cts_name NVARCHAR(50), cts_full_descriptor	NVARCHAR(MAX), industry NVARCHAR(MAX),	scale NVARCHAR(10),
   [1995] FLOAT,	[1996] FLOAT,	[1997] FLOAT,	[1998] FLOAT,	[1999] FLOAT,	[2000] FLOAT,	[2001] FLOAT,	[2002] FLOAT,	[2003] FLOAT,	[2004] FLOAT,	[2005] FLOAT,	[2006] FLOAT,
   [2007] FLOAT,	[2008] FLOAT,	[2009] FLOAT,	[2010] FLOAT,	[2011] FLOAT,	[2012] FLOAT,	[2013] FLOAT,	[2014] FLOAT,	[2015] FLOAT,	[2016] FLOAT,	[2017] FLOAT,	[2018] FLOAT,
 
