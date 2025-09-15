8d7084b674d0b62cc2703cd55e09229d4392e640
Ric Araojo <ojoararico.me@gmail.com>
2025-09-15 10:44:38 +0800

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -15,8 +15,10 @@ IF OBJECT_ID('silver.ghg_co2_emissions', 'U') IS NOT NUll
 SELECT
   country,	iso2,	iso3,	indicator_used, unit, 
   cts_code,	industry,
-  [1995],	[1996],	[1997],	[1998],	[1999],	[2000],	[2001],	[2002],	[2003],	[2004],	[2005],	[2006],
-  [2007],	[2008],	[2009],	[2010],	[2011],	[2012],	[2013],	[2014],	[2015],	[2016],	[2017],	[2018]
+  CAST([1995] AS FLOAT) AS [1995],	CAST([1996] AS FLOAT) AS [1996],	CAST([1997] AS FLOAT) AS [1997],	CAST([1998] AS FLOAT) AS [1998],	CAST([1999] AS FLOAT) AS [1999],	CAST([2000] AS FLOAT) AS [2000],	
+  CAST([2001] AS FLOAT) AS [2001],	CAST([2002] AS FLOAT) AS [2002],	CAST([2003] AS FLOAT) AS [2003],	CAST([2004] AS FLOAT) AS [2004],	CAST([2005] AS FLOAT) AS [2005],	CAST([2006] AS FLOAT) AS [2006],
+  CAST([2007] AS FLOAT) AS [2007],	CAST([2008] AS FLOAT) AS [2008],	CAST([2009] AS FLOAT) AS [2009],	CAST([2010] AS FLOAT) AS [2010],	CAST([2011] AS FLOAT) AS [2011],	CAST([2012] AS FLOAT) AS [2012],	
+  CAST([2013] AS FLOAT) AS [2013],	CAST([2014] AS FLOAT) AS [2014],	CAST([2015] AS FLOAT) AS [2015],	CAST([2016] AS FLOAT) AS [2016],	CAST([2017] AS FLOAT) AS [2017],	CAST([2018] AS FLOAT) AS [2018]
 INTO silver.ghg_co2_emissions
 FROM bronze.ghg_co2_emissions
 GO
@@ -28,9 +30,11 @@ IF OBJECT_ID('silver.ghg_carbon_footprints', 'U') IS NOT NUll
 SELECT
   country,	iso2,	iso3,	indicator_used, 
   unit, cts_code,
-  [1995],	[1996],	[1997],	[1998],	[1999],	[2000],	[2001],	[2002],	[2003],	[2004],	[2005],	[2006],
-  [2007],	[2008],	[2009],	[2010],	[2011],	[2012],	[2013],	[2014],	[2015],	[2016],	[2017],	[2018],
-  [2019], [2020],   [2021]
+  CAST([1995] AS FLOAT) AS [1995],	CAST([1996] AS FLOAT) AS [1996],	CAST([1997] AS FLOAT) AS [1997],	CAST([1998] AS FLOAT) AS [1998],	CAST([1999] AS FLOAT) AS [1999],	CAST([2000] AS FLOAT) AS [2000],	
+  CAST([2001] AS FLOAT) AS [2001],	CAST([2002] AS FLOAT) AS [2002],	CAST([2003] AS FLOAT) AS [2003],	CAST([2004] AS FLOAT) AS [2004],	CAST([2005] AS FLOAT) AS [2005],	CAST([2006] AS FLOAT) AS [2006],
+  CAST([2007] AS FLOAT) AS [2007],	CAST([2008] AS FLOAT) AS [2008],	CAST([2009] AS FLOAT) AS [2009],	CAST([2010] AS FLOAT) AS [2010],	CAST([2011] AS FLOAT) AS [2011],	CAST([2012] AS FLOAT) AS [2012],	
+  CAST([2013] AS FLOAT) AS [2013],	CAST([2014] AS FLOAT) AS [2014],	CAST([2015] AS FLOAT) AS [2015],	CAST([2016] AS FLOAT) AS [2016],	CAST([2017] AS FLOAT) AS [2017],	CAST([2018] AS FLOAT) AS [2018],
+  CAST([2019] AS FLOAT) AS [2019],  CAST([2020] AS FLOAT) AS [2020],    CAST([2021] AS FLOAT) AS [2021]
 INTO silver.ghg_carbon_footprints
 FROM bronze.ghg_carbon_footprints
 
@@ -39,10 +43,9 @@ IF OBJECT_ID('silver.ghg_investment_related', 'U') IS NOT NUll
 	DROP TABLE silver.ghg_investment_related; 
   
 SELECT
-  country,	iso2,	iso3,	indicator_used, 
-  unit, cts_code, sector,	
-  [2005],	[2006], [2007],	[2008],	[2009], [2010],	
-  [2011],	[2012],	[2013],	[2014],	[2015],	[2016] 
+  country,	iso2,	iso3,	indicator_used, unit, cts_code, sector,	
+  CAST([2005] AS FLOAT) AS [2005],	CAST([2006] AS FLOAT) AS [2006], CAST([2007] AS FLOAT) AS [2007],	CAST([2008] AS FLOAT) AS [2008],	CAST([2009] AS FLOAT) AS [2009],	CAST([2010] AS FLOAT) AS [2010],	
+  CAST([2011] AS FLOAT) AS [2011],	CAST([2012] AS FLOAT) AS [2012], CAST([2013] AS FLOAT) AS [2013],	CAST([2014] AS FLOAT) AS [2014],	CAST([2015] AS FLOAT) AS [2015],	CAST(TRIM(REPLACE([2016], ',', '')) AS FLOAT) AS [2016]
 INTO silver.ghg_investment_related
 FROM bronze.ghg_investment_related
 
